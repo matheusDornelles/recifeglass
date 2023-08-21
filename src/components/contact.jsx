7696e587737846755cc0fadde0721495341c4b99
Eduardo Vieira <carlosevhs@gmail.com>
2023-08-21 11:25:06 -0300

    alterando seção nossos serviços

--- src/components/contact.jsx
+++ src/components/contact.jsx
@@ -38,10 +38,9 @@ export const Contact = (props) => {
           <div className="col-md-8">
             <div className="row">
               <div className="section-title">
-                <h2>Get In Touch</h2>
+                <h2>Faça ja seu orçamento</h2>
                 <p>
-                  Please fill out the form below to send us an email and we will
-                  get back to you as soon as possible.
+                  
                 </p>
               </div>
               <form name="sentMessage" validate onSubmit={handleSubmit}>
